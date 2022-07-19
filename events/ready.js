3ca32f5576472d06e87ab01a285700805e1ebdbe
KuroNeko <20273059+imkuroneko@users.noreply.github.com>
2022-07-19 13:41:35 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,8 @@
+const { ActivityType } = require('discord.js');
+
 module.exports = {
     name: 'ready',
     execute(guild) {
-        guild.user.setActivity("los mensajes enviados", { type: "WATCHING" });
+        guild.user.setActivity("los mensajes enviados", { type: ActivityType.Watching });
     }
 };
